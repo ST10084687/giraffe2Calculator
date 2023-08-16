0f11e3c968f97f8fdbf099ab0c3ed9bbbaeb8be6
ST10084687 <105341673+ST10084687@users.noreply.github.com>
2023-08-16 17:49:33 +0200

    Add files via upload

---
+++ giraffe2CalculatorProject/giraffe2CalculatorProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giraffe2CalculatorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value One: ");
+            int valOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value Two: ");
+            int valTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The two numbers added together: " + (valOne + valTwo));
+           
+
+            Console.ReadLine();
+        }
+    }
+}
